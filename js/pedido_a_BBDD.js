ee488828c1fbf86d8d1a2b69f2a47a51abb29e70
Alberto <aandgom@etsinf.upv.es>
2024-05-28 18:02:45 +0200

    s

--- js/pedido_a_BBDD.js
+++ js/pedido_a_BBDD.js
@@ -1,7 +1,7 @@
 document. getElementById("confirmar-compra").addEventListener("click", async ()=> {
     const carrito = JSON.parse(localStorage.getItem("products"));
     if(carrito && carrito.length > 0){
-        const res = await fetch("http://steady-mermaid-8a8d30.netlify.app/carrito","https://steady-mermaid-8a8d30.netlify.app/carrito",{
+        const res = await fetch("http://steady-mermaid-8a8d30.netlify.app/carrito",{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
