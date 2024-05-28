c818e8831f579452422f1fc390cdcbfd6b637d46
Alberto <aandgom@etsinf.upv.es>
2024-05-28 20:27:12 +0200

    ss

--- js/pedido_a_BBDD.js
+++ js/pedido_a_BBDD.js
@@ -2,7 +2,7 @@ document. getElementById("confirmar-compra").addEventListener("click", async ()=
     const carrito = JSON.parse(localStorage.getItem("products"));
     console.log(carrito);
     if(carrito && carrito.length > 0){
-            const res = await fetch("http://localhost:4000/carrito",{
+            const res = await fetch("http://localhost:4000/carrito/comprar",{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -13,7 +13,7 @@ document. getElementById("confirmar-compra").addEventListener("click", async ()=
             console.log("Compra realizada");
         }
         else{
-            //window.location.href = "fallo_conexion.html";
+            window.location.href = "fallo_conexion.html";
         }
     }
 });
