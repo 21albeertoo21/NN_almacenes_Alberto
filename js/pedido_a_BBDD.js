6e7b9069a9ca58886c647f09fdf3fd256fe2613a
Alberto <aandgom@etsinf.upv.es>
2024-05-28 20:17:34 +0200

    s

--- js/pedido_a_BBDD.js
+++ js/pedido_a_BBDD.js
@@ -1,5 +1,6 @@
 document. getElementById("confirmar-compra").addEventListener("click", async ()=> {
     const carrito = JSON.parse(localStorage.getItem("products"));
+    console.log(carrito);
     if(carrito && carrito.length > 0){
             const res = await fetch("http://localhost:4000/carrito",{
             method: "POST",
